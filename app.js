92466dabeafddfadb430754eaa12c8aa18d9ca81
Nhật Nguyễn Đình <nhatnguyendinh@Nhats-MacBook-Air.local>
2022-07-13 22:15:02 +0700

    fist

---
+++ app.js
@@ -0,0 +1,9 @@
+// var btnopen = document.querySelector('.login-but')
+// var form = document.querySelector('.login')
+// var cancel = document.querySelector('.close')
+// function toggleform()
+// {
+//     form.classList.toggle('hide')
+// }
+// btnopen.addEventListener('click', toggleform)
+// var login = document.querySelector('Register_but')
