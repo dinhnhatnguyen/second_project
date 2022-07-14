beef66e0d1b917465d1e9aebef426e6775fe3d82
Nhật Nguyễn Đình <nhatnguyendinh@Nhats-MacBook-Air.local>
2022-07-14 08:09:08 +0700

    second

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // var btnopen = document.querySelector('.login-but')
 // var form = document.querySelector('.login')
 // var cancel = document.querySelector('.close')
+// document.getElementsByClassName(login-but).on
 // function toggleform()
-// {
-//     form.classList.toggle('hide')
-// }
-// btnopen.addEventListener('click', toggleform)
+//
+    // document.getElementsByClassName(hide).
+    // form.classList.toggle('hide')
+//}
 // var login = document.querySelector('Register_but')
